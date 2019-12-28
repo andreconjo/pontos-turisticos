c4d4b74f62ffc0108e04fe06e940df5278def56c
andreconjo <andre.henrique@alu.ufc.br>
2019-12-28 17:09:19 -0300

    Adicionado foto ao ponto turistico

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -7,3 +7,21 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
